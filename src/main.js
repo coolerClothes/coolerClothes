806670277224bf4dba14e8be6c8e3c41903ea649
Lucia <lucia.macakova@iths.se>
2024-02-20 13:29:16 +0100

    first draft on router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,24 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import './style.css'
 import App from './App.vue'
+import Home from './home.vue'
+import About from './about.vue'
 
-createApp(App).mount('#app')
+
+const router = createRouter({
+    history: createWebHashHistory,
+    routes:[
+        {
+            path:"/", name: "Home", component: Home
+        },
+        {
+            path:"/about", name: "About", component: About
+        },
+    ]
+
+})
+
+
+
+createApp(App).use(router).mount('#app')
