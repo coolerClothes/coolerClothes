fa96f66f6b9912d11f32bd014efd69f752398dda
Andrei Pirogov Huertas <andrei.pirogov.huertas@gmail.com>
2024-02-21 11:16:32 +0100

    Fixed package-lock.json Added fetch to json, uploaded prods to pinia,

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,33 @@
+<script setup>
+import { useProductsStore } from "./store";
 
-<script>
+const store = useProductsStore();
+
+const fetchProducts = async () => {
+  try {
+    await fetch("../public/products.json")
+      .then((response) => response.json())
+      .then((result) => {
+        store.getProducts(result);
+      });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+fetchProducts();
 </script>
 
 <template>
   <div>
-    <ul> router links:
-    <li><router-link to="/">home page</router-link></li>
-    <li><router-link to="/search">search result</router-link></li>
-    <li><router-link to="/product">product pages</router-link></li>
-  </ul>
+    <ul>
+      router links:
+      <li><router-link to="/">home page</router-link></li>
+      <li><router-link to="/search">search result</router-link></li>
+      <li><router-link to="/product">product pages</router-link></li>
+    </ul>
     <router-view></router-view>
-
   </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
