1e31e3b5af1cf760f2c89553bcbc572ae40e6f98
Amanda Ruiz <98482837+amilpsp@users.noreply.github.com>
2024-02-21 11:04:49 +0100

    inner grid for product info

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -5,11 +5,10 @@
         :src="cardImgSrc"
         class="overflow-hidden w-full h-32 sm:h-48 object cover"
       />
-      <div id="product-info" class="p-4">
+      <div id="product-info" class="p-4 grid-cols-3 grid-rows-2">
         <h3>{{ product.title }}</h3>
-        <h2>SEK{{ product.price }}</h2>
-        <button id="add-to-cart" class="uppercase rounded"></button>
-        <button id="read-more" class="uppercase rounded"></button>
+        <h4>{{ product.brand }}</h4>
+        <h2 class="row-span-2">SEK{{ product.price }}</h2>
       </div>
       <!-- product-info -->
     </div>
