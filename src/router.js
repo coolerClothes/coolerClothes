2dc221536a2e649b1aa38155304bb03c60d646cd
Lucia <lucia.macakova@iths.se>
2024-02-22 10:01:58 +0100

    edited routes

--- src/router.js
+++ src/router.js
@@ -2,18 +2,13 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import Home from './views/home.vue'
 import Search from './views/search.vue'
 import Product from './views/product.vue'
-import Filtered_view from './views/filtered_view.vue'
-
 
 const routes = [
     {
         path: "/", name: "Home", component: Home
     },
     {
-        path: "/search", name: "Search", component: Search
-    },
-    {
-        path: "/search/:category", name: "Filter", component: Filtered_view
+        path: "/search/:category/:searchquery", name: "Search", component: Search
     },
     {
         path: "/:product", name: "Product", component: Product
