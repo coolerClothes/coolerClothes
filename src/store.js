ee4c159fcd4ef55769d2279e6f2027ae192720e7
Andrei Pirogov Huertas <andrei.pirogov.huertas@gmail.com>
2024-02-22 14:41:28 +0100

    removed product filter action

--- src/store.js
+++ src/store.js
@@ -5,9 +5,6 @@ export const useProductsStore = defineStore("products", {
     getProducts(data) {
       this.productsCatalogue = data;
     },
-    getProductByName(name) {
-      return this.productsCatalogue.find((product) => product.name === name);
-    },
   },
   state: () => ({ productsCatalogue: null }),
 });
