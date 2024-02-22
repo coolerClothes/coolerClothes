440abb47fc66a1ec7818f0ed5ca0c2de2d7a36f0
Amanda Ruiz <98482837+amilpsp@users.noreply.github.com>
2024-02-22 10:52:31 +0100

    hidden overflow to be only on x axis

--- src/views/product.vue
+++ src/views/product.vue
@@ -2,7 +2,7 @@
 import ProductInfo from "../components/ProductInfo.vue";
 </script>
 <template>
-  <div class="grid grid-cols-10 space-x-6 px-24 my-4 overflow-hidden">
+  <div class="grid grid-cols-10 space-x-6 px-24 my-4 overflow-x-hidden">
     <!-- <h1 class="col-span-10">product page view. {{ $route.params.product }}</h1> -->
     <ProductInfo />
   </div>
