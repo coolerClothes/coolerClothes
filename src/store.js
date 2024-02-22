ca4894add3974366e92de4585b29660d64250b08
Andrei Pirogov Huertas <andrei.pirogov.huertas@gmail.com>
2024-02-22 14:36:21 +0100

    Added new action to store.js

--- src/store.js
+++ src/store.js
@@ -5,6 +5,9 @@ export const useProductsStore = defineStore("products", {
     getProducts(data) {
       this.productsCatalogue = data;
     },
+    getProductByName(name) {
+      return this.productsCatalogue.find((product) => product.name === name);
+    },
   },
   state: () => ({ productsCatalogue: null }),
 });
