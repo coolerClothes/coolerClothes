44daac1aac630f52256cf5ae43951cf1130c0f68
Amanda Ruiz <98482837+amilpsp@users.noreply.github.com>
2024-02-22 12:35:33 +0100

    imported pinia store to try to display cards

--- src/views/product.vue
+++ src/views/product.vue
@@ -1,9 +1,23 @@
 <script setup>
 import ProductInfo from "../components/ProductInfo.vue";
+import Card from "../components/Card.vue";
+import { useProductsStore } from "../store";
+
+const store = useProductsStore();
+const products = store.productsCatalogue;
 </script>
 <template>
   <div class="px-24 my-4 overflow-x-hidden">
     <!-- <h1 class="col-span-10">product page view. {{ $route.params.product }}</h1> -->
     <ProductInfo />
+    <div class="grid grid-cols-5 gap-4 p-4">
+      <Card
+        class="col-span-1"
+        v-for="product in products"
+        :key="product.id"
+        :product="product"
+        :cardImgSrc="product.imgSrc"
+      />
+    </div>
   </div>
 </template>
