48ed49b269753d522aa538576962697c7057aaae
Amanda Ruiz <98482837+amilpsp@users.noreply.github.com>
2024-02-23 19:57:46 +0100

    propper syntax for the link interpolation

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -13,7 +13,9 @@
 
 <template>
   <div id="card-background" class="rounded shadow overflow-hidden">
-    <img :src="cardImgSrc" class="w-full h-32 sm:h-48 object-cover" />
+    <RouterLink :to="`/${product.title}`">
+      <img :src="cardImgSrc" class="w-full h-32 sm:h-48 object-cover" />
+    </RouterLink>
     <div id="product-info" class="grid grid-cols-3 grid-rows-2 p-4">
       <h3 class="col-span-2 row-span-1">{{ product.title }}</h3>
       <h4 class="col-span-2 row-span-1">{{ product.brand }}</h4>
