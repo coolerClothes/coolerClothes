c2bee902dd51deb03381e49b25ed4c01e06ef5d4
Amanda Ruiz <98482837+amilpsp@users.noreply.github.com>
2024-02-23 22:25:20 +0100

    tried to pass galleryImgSrc as route param as well, added props for another possible method. To no avail

--- src/router.js
+++ src/router.js
@@ -1,23 +1,30 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import Home from './views/home.vue'
-import Search from './views/search.vue'
-import Product from './views/product.vue'
+import { createRouter, createWebHashHistory } from "vue-router";
+import Home from "./views/home.vue";
+import Search from "./views/search.vue";
+import Product from "./views/product.vue";
 
 const routes = [
-    {
-        path: "/", name: "Home", component: Home
-    },
-    {
-        path: "/search/:category/:searchquery?", name: "Search", component: Search,
-    },
-    {
-        path: "/products/:product", name: "Product", component: Product
-    }
-]
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/search/:category/:searchquery?",
+    name: "Search",
+    component: Search,
+  },
+  {
+    path: "/products/:product/:galleryImgSrc",
+    name: "Product",
+    component: Product,
+    props: true,
+  },
+];
 
 const router = createRouter({
-    history: createWebHashHistory(),
-    routes
-})
+  history: createWebHashHistory(),
+  routes,
+});
 
-export default router
+export default router;
