6f29307225a454e50072f341f60a54c048ec3e47
Amanda Ruiz <98482837+amilpsp@users.noreply.github.com>
2024-02-28 14:53:17 +0100

    Prettier dev dependency: run "npm install" after pull (#100)
    * counter & buttons on add to cart, weird bug
    
    * fixed but, broke placement
    
    * v-model and submit
    
    * installed Prettier as a dev dependency and created a config file

--- src/components/ProductInfo.vue
+++ src/components/ProductInfo.vue
@@ -7,8 +7,6 @@
       id="gallery-container"
       class="p-4 space-y-2 col-span-5 lg:col-span-7 bg-[#1c1c1c] flex flex-col lg:h-auto lg:max-h-screen justify-around items-center"
     >
-      <!-- <div class="imgContainer ">
-      </div> -->
       <img :src="galleryImgSrc" class="max-h-80 overflow-hidden object-cover" />
 
       <div
@@ -38,10 +36,22 @@
     >
       <div id="titles-and-stock" class="relative">
         <h3 class="text-xl font-bold">{{ product.title }}</h3>
-        <span
-          class="rounded p-1 bg-slate-100 absolute top-1/4 right-0 text-black"
-          >in stock</span
-        >
+        <div class="absolute bottom-1/4 right-0 flex flex-row">
+          <span class="flex items-center p-1 text-white"
+            ><svg
+              class="h-4 w-4 text-emerald-500 mr-1"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <circle cx="12" cy="12" r="10" />
+            </svg>
+            in stock</span
+          >
+        </div>
         <h4 class="text-[#a3a3a3]">{{ product.brand }}</h4>
       </div>
       <!-- titles and stock -->
@@ -59,19 +69,52 @@
         </span>
       </div>
       <!-- desrciption container -->
-      <form id="add-to-cart" class="space-y-2">
-        <input
-          type="text"
-          name="Size"
-          list="sizeList"
-          class="rounded w-full my-1.5 text-center text-black"
-        />
-        <datalist id="sizeList">
-          <option v-for="size in product.sizes" :value="size"></option>
-        </datalist>
+
+      <div
+        id="counter-container"
+        class="flex w-full items-center justify-around"
+      >
         <button
+          name="decrease"
+          @click="decrease()"
+          label="-"
+          class="px-2 rounded-lg font-semibold bg-[#a3a3a3] text-black"
+        >
+          -
+        </button>
+        <span class="">{{ count }}</span>
+        <button
+          name="increase"
+          @click="increase()"
+          label="+"
+          class="px-2 rounded-lg font-semibold bg-[#a3a3a3] text-black"
+        >
+          +
+        </button>
+      </div>
+      <!-- counter container -->
+
+      <form id="add-to-cart" class="space-y-2">
+        <div
+          id="size-and-amount-container"
+          class="flex flex-row justify-around items-center"
+        >
+          <input
+            type="text"
+            v-model="selectedSize"
+            name="Size"
+            list="sizeList"
+            class="h-8 rounded-lg my-1.5 text-center bg-[#a3a3a3] text-black"
+          />
+          <datalist id="sizeList">
+            <option v-for="size in product.sizes" :value="size"></option>
+          </datalist>
+        </div>
+        <!-- size and amount container -->
+        <button
+          type="submit"
           label="Add to cart"
-          class="w-full bg-[#FF007A] text-white rounded-full p-0.5"
+          class="h-8 w-full duration-300 ease-in-out bg-black hover:bg-[#FF007A] text-white hover:text-black font-bold rounded-full p-0.5"
         >
           Add to cart
         </button>
@@ -81,6 +124,18 @@
   </div>
   <!-- component container -->
 </template>
+<script setup>
+import { ref } from "vue";
+
+const count = ref(1);
+const increase = () => {
+  count.value++;
+};
+const decrease = () => {
+  count.value--;
+};
+</script>
+
 <script>
 export default {
   props: {
