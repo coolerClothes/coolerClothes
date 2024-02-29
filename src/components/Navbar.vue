b70ecdbc3a902da865b9b9b0c1983bf441a355bc
celoob <carlemilloob@gmail.com>
2024-02-29 13:32:56 +0100

    Logo interactivity fixes, Slim navbar for scrolling, Extra logo versions for big and small nav + hover versions

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,13 +1,16 @@
 <script setup>
-import { ref } from "vue";
+import { ref, computed, onMounted, onBeforeUnmount } from "vue";
 import SideMenu from "./SideMenu.vue";
 import CartMenu from "./CartMenu.vue";
 import { useRouter } from "vue-router";
+
 const sideMenuActive = ref(false);
 const cartMenuActive = ref(false);
 const searchInput = ref("");
+const isScrolled = ref(false);
 
 const router = useRouter();
+
 const handleSideMenuActivation = () => {
   sideMenuActive.value = !sideMenuActive.value;
 };
@@ -20,10 +23,40 @@ const handleSearch = () => {
   router.push({ path: "/search/all/" + searchInput.value });
   console.log(searchInput.value);
 };
+
+// Handle scroll event
+const handleScroll = () => {
+  isScrolled.value = window.scrollY > 0;
+};
+
+// Add event listeners on component mount
+onMounted(() => {
+  window.addEventListener("scroll", handleScroll);
+});
+
+// Remove event listeners on component unmount
+onBeforeUnmount(() => {
+  window.removeEventListener("scroll", handleScroll);
+});
+
+const logoSrc = computed(() => {
+  return isScrolled.value
+    ? "/src/assets/small-logo.png" // Use this logo when scrolled
+    : "/src/assets/logo.png"; // Use the default logo when not scrolled
+});
+const logoAltSrc = computed(() => {
+  return isScrolled.value
+    ? "/src/assets/small-alt-logo.png" // Use this logo when scrolled
+    : "/src/assets/alt-logo.png"; // Use the default logo when not scrolled
+});
+
+const bottomContainerClass = computed(() => {
+  return isScrolled.value ? "hidden" : "";
+});
 </script>
 
 <template>
-  <div id="navbar" class="z-50 text-[#f5f5f5] top-0">
+  <div id="navbar" class="z-50 text-[#f5f5f5] top-0 sticky">
     <SideMenu
       :side-menu-active="sideMenuActive"
       @handle-side-menu-activation="handleSideMenuActivation"
@@ -34,7 +67,11 @@ const handleSearch = () => {
       @handle-cart-menu-activation="handleCartMenuActivation"
     />
 
-    <div id="navbar-top-container" class="flex items-center bg-[#1c1c1c] p-3">
+    <div
+      id="navbar-top-container"
+      class="flex items-center bg-[#1c1c1c] p-3 transition-all h-32 w-full"
+      :class="{ 'slim-navbar': isScrolled }"
+    >
       <div id="navbar-left" class="flex-1 flex justify-center">
         <div id="input-container" class="relative max-md:hidden">
           <input
@@ -72,13 +109,13 @@ const handleSearch = () => {
         <div id="logo-container" class="mx-5 relative">
           <router-link to="/">
             <img
-              src="/src/assets/logo.png"
+              :src="logoSrc"
               alt="Cooler Clothes logo"
               class="max-h-24 max-sm:my-3 transition duration-300 ease-in-out transform hover:scale-105"
               id="logo1"
             />
             <img
-              src="/src/assets/alt-logo.png"
+              :src="logoAltSrc"
               alt="Cooler Clothes logo"
               class="max-h-24 max-sm:my-3 transition duration-300 ease-in-out opacity-0 absolute top-0 left-0 transform hover:scale-105"
               id="logo2"
@@ -122,6 +159,7 @@ const handleSearch = () => {
     <div
       id="navbar-bottom-container"
       class="h-9 max-md:hidden bg-[#0c0c0c] p-5"
+      :class="bottomContainerClass"
     >
       <ul
         class="flex gap-5 h-full justify-center items-center font-inter font-semibold text-xs"
@@ -163,5 +201,31 @@ const handleSearch = () => {
 
 #logo-container:hover #logo2 {
   opacity: 1;
+}
+
+.slim-navbar #logo1 {
+  height: 3rem;
+}
+
+#navbar-top-container {
+  transition:
+    height 0.3s ease-in-out,
+    background-color 0.3s ease-in-out;
+}
+
+.slim-navbar {
+  height: 4rem;
+  padding-top: 2rem;
+  padding-bottom: 2rem;
+  background-color: #1c1c1c; /* Adjust the color as needed */
+  border-bottom: 5px solid #0c0c0c;
+}
+
+/* #navbar-bottom-container {
+  transition: height 0.3s ease-in-out;
+} */
+
+.slim-navbar #navbar-bottom-container {
+  height: 0;
 }
 </style>
