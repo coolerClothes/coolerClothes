6551fe1671e7019995749cbd1b86213c6eb21fb7
celoob <carlemilloob@gmail.com>
2024-02-29 10:57:01 +0100

    Card, footer typography. Logo hover animations

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,162 +1,167 @@
 <script setup>
-    import { ref } from 'vue'
-    import SideMenu from './SideMenu.vue'
-    import CartMenu from './CartMenu.vue'
-    import { useRouter } from 'vue-router'
-    const sideMenuActive = ref(false)
-    const cartMenuActive = ref(false)
-    const searchInput = ref('')
+import { ref } from "vue";
+import SideMenu from "./SideMenu.vue";
+import CartMenu from "./CartMenu.vue";
+import { useRouter } from "vue-router";
+const sideMenuActive = ref(false);
+const cartMenuActive = ref(false);
+const searchInput = ref("");
 
-    const router = useRouter()
-    const handleSideMenuActivation = () => {
-        sideMenuActive.value = !sideMenuActive.value
-    }
+const router = useRouter();
+const handleSideMenuActivation = () => {
+  sideMenuActive.value = !sideMenuActive.value;
+};
 
-    const handleCartMenuActivation = () => {
-        cartMenuActive.value = !cartMenuActive.value
-    }
+const handleCartMenuActivation = () => {
+  cartMenuActive.value = !cartMenuActive.value;
+};
 
-    const handleSearch = () => {
-        router.push({ path: '/search/all/' + searchInput.value })
-        console.log(searchInput.value)
-    }
+const handleSearch = () => {
+  router.push({ path: "/search/all/" + searchInput.value });
+  console.log(searchInput.value);
+};
 </script>
 
 <template>
-    <div id="navbar" class="z-50 text-[#f5f5f5] top-0">
-        <SideMenu
-            :side-menu-active="sideMenuActive"
-            @handle-side-menu-activation="handleSideMenuActivation"
-        />
+  <div id="navbar" class="z-50 text-[#f5f5f5] top-0">
+    <SideMenu
+      :side-menu-active="sideMenuActive"
+      @handle-side-menu-activation="handleSideMenuActivation"
+    />
 
-        <CartMenu
-            :cart-menu-active="cartMenuActive"
-            @handle-cart-menu-activation="handleCartMenuActivation"
-        />
+    <CartMenu
+      :cart-menu-active="cartMenuActive"
+      @handle-cart-menu-activation="handleCartMenuActivation"
+    />
+
+    <div id="navbar-top-container" class="flex items-center bg-[#1c1c1c] p-3">
+      <div id="navbar-left" class="flex-1 flex justify-center">
+        <div id="input-container" class="relative max-md:hidden">
+          <input
+            type="text"
+            class="w-64 h-10 rounded-full pl-10 text-black focus:outline-[#1e1e1e]"
+            placeholder="Search"
+            v-model="searchInput"
+            @keyup.enter="handleSearch"
+          />
+          <img
+            src="/src/assets/icons/search-icon.svg"
+            alt=""
+            class="absolute transition-all z-10 duration-500 pl-2 top-2 h-6"
+          />
+        </div>
 
         <div
-            id="navbar-top-container"
-            class="flex items-center bg-[#1c1c1c] p-3"
+          id="left-side-mobile"
+          class="flex max-sm:gap-3 gap-6 w-full ml-5 justify-start md:hidden"
         >
-            <div id="navbar-left" class="flex-1 flex justify-center">
-                <div id="input-container" class="relative max-md:hidden">
-                    <input
-                        type="text"
-                        class="w-64 h-10 rounded-full pl-10 text-black"
-                        placeholder="Search"
-                        v-model="searchInput"
-                        @keyup.enter="handleSearch"
-                    />
-                    <img
-                        src="/src/assets/icons/search-icon.svg"
-                        alt=""
-                        class="absolute transition-all z-10 duration-500 pl-2 top-2 h-6"
-                    />
-                </div>
-
-                <div
-                    id="left-side-mobile"
-                    class="flex max-sm:gap-3 gap-6 w-full ml-5 justify-start md:hidden"
-                >
-                    <img
-                        src="/src/assets/icons/hamburger-icon.svg"
-                        alt=""
-                        class="h-6 cursor-pointer"
-                        @click="handleSideMenuActivation"
-                    />
-                    <img
-                        src="/src/assets/icons/search-icon-black.svg"
-                        alt=""
-                        class="h-6"
-                    />
-                </div>
-            </div>
-            <div id="navbar-center" class="flex flex-1 justify-center">
-                <div id="logo-container" class="mx-5">
-                    <router-link to="/"
-                        ><img
-                            src="/src/assets/logo.png"
-                            alt="Cooler Clothes logo"
-                            class="max-h-24 max-sm:my-3"
-                    /></router-link>
-                </div>
-            </div>
-            <div
-                id="navbar-right"
-                class="flex flex-1 justify-center gap-4 max-sm:gap-5 max-md:gap-9 max-md:justify-end max-md:mr-5 text-sm"
-            >
-                <div class="flex pr-1">
-                    <img
-                        src="/src/assets/icons/login-icon.svg"
-                        alt="mask icon"
-                        class="min-w-6"
-                    />
-                    <h2 class="pl-1 pt-0.5 max-md:hidden">Login</h2>
-                </div>
-                <div class="flex pr-1">
-                    <img
-                        src="/src/assets/icons/favorite-icon.svg"
-                        alt="heart icon"
-                        class="min-w-6"
-                    />
-                    <h2 class="pl-1 pt-0.5 max-md:hidden">Favorites</h2>
-                </div>
-                <div
-                    class="flex cursor-pointer pr-1"
-                    @click="handleCartMenuActivation"
-                >
-                    <img
-                        src="/src/assets/icons/cart-icon.svg"
-                        alt="Shopping bag icon"
-                        class="min-w-6"
-                    />
-                    <h2 class="pl-1 pt-0.5 max-md:hidden">Cart</h2>
-                </div>
-            </div>
+          <img
+            src="/src/assets/icons/hamburger-icon.svg"
+            alt=""
+            class="h-6 cursor-pointer"
+            @click="handleSideMenuActivation"
+          />
+          <img
+            src="/src/assets/icons/search-icon-black.svg"
+            alt=""
+            class="h-6"
+          />
+        </div>
+      </div>
+      <div id="navbar-center" class="flex flex-1 justify-center">
+        <div id="logo-container" class="mx-5 relative">
+          <router-link to="/">
+            <img
+              src="/src/assets/logo.png"
+              alt="Cooler Clothes logo"
+              class="max-h-24 max-sm:my-3 transition duration-300 ease-in-out transform hover:scale-105"
+              id="logo1"
+            />
+            <img
+              src="/src/assets/alt-logo.png"
+              alt="Cooler Clothes logo"
+              class="max-h-24 max-sm:my-3 transition duration-300 ease-in-out opacity-0 absolute top-0 left-0 transform hover:scale-105"
+              id="logo2"
+            />
+          </router-link>
+        </div>
+      </div>
+      <div
+        id="navbar-right"
+        class="flex flex-1 justify-center gap-4 max-sm:gap-5 max-md:gap-9 max-md:justify-end max-md:mr-5 text-sm"
+      >
+        <div class="flex pr-1 hover:text-[#ff007a]">
+          <img
+            src="/src/assets/icons/login-icon.svg"
+            alt="mask icon"
+            class="min-w-6"
+          />
+          <h2 class="pl-1 pt-0.5 max-md:hidden">Login</h2>
+        </div>
+        <div class="flex pr-1 hover:text-[#ff007a]">
+          <img
+            src="/src/assets/icons/favorite-icon.svg"
+            alt="heart icon"
+            class="min-w-6"
+          />
+          <h2 class="pl-1 pt-0.5 max-md:hidden">Favorites</h2>
         </div>
         <div
-            id="navbar-bottom-container"
-            class="h-9 max-md:hidden bg-[#0c0c0c] p-5"
+          class="flex cursor-pointer pr-1 hover:text-[#ff007a]"
+          @click="handleCartMenuActivation"
         >
-            <ul
-                class="flex gap-5 h-full justify-center items-center font-inter font-semibold text-xs"
-            >
-                <li>
-                    <router-link
-                        to="/search/jackets"
-                        class="p-4 hover:text-[#ff007a]"
-                        >JACKETS</router-link
-                    >
-                </li>
-                <li>
-                    <router-link
-                        to="/search/hoodies"
-                        class="p-4 hover:text-[#ff007a]"
-                        >HOODIES</router-link
-                    >
-                </li>
-                <li>
-                    <router-link
-                        to="/search/shirts"
-                        class="p-4 hover:text-[#ff007a]"
-                        >SHIRTS</router-link
-                    >
-                </li>
-                <li>
-                    <router-link
-                        to="/search/pants"
-                        class="p-4 hover:text-[#ff007a]"
-                        >PANTS</router-link
-                    >
-                </li>
-                <li>
-                    <router-link
-                        to="/search/accessories"
-                        class="p-4 hover:text-[#ff007a]"
-                        >ACCESSORIES</router-link
-                    >
-                </li>
-            </ul>
+          <img
+            src="/src/assets/icons/cart-icon.svg"
+            alt="Shopping bag icon"
+            class="min-w-6"
+          />
+          <h2 class="pl-1 pt-0.5 max-md:hidden">Cart</h2>
         </div>
+      </div>
     </div>
+    <div
+      id="navbar-bottom-container"
+      class="h-9 max-md:hidden bg-[#0c0c0c] p-5"
+    >
+      <ul
+        class="flex gap-5 h-full justify-center items-center font-inter font-semibold text-xs"
+      >
+        <li>
+          <router-link to="/search/jackets" class="p-4 hover:text-[#ff007a]"
+            >JACKETS</router-link
+          >
+        </li>
+        <li>
+          <router-link to="/search/hoodies" class="p-4 hover:text-[#ff007a]"
+            >HOODIES</router-link
+          >
+        </li>
+        <li>
+          <router-link to="/search/shirts" class="p-4 hover:text-[#ff007a]"
+            >SHIRTS</router-link
+          >
+        </li>
+        <li>
+          <router-link to="/search/pants" class="p-4 hover:text-[#ff007a]"
+            >PANTS</router-link
+          >
+        </li>
+        <li>
+          <router-link to="/search/accessories" class="p-4 hover:text-[#ff007a]"
+            >ACCESSORIES</router-link
+          >
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
+
+<style scoped>
+#logo-container:hover #logo1 {
+  opacity: 0;
+}
+
+#logo-container:hover #logo2 {
+  opacity: 1;
+}
+</style>
