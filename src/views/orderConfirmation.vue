073bcf2549c7e6812477df41cd433e429c5507e5
Amanda Ruiz <98482837+amilpsp@users.noreply.github.com>
2024-03-07 17:11:28 +0100

    need to switch branches

--- src/views/orderConfirmation.vue
+++ src/views/orderConfirmation.vue
@@ -3,13 +3,13 @@
     id="component-container"
     class="flex flex-col place-items-center place-content-center min-h-[80vh] space-y-3"
   >
-    <h2 id="thank-you" class="text-2xl font-inter tracking-tight">
+    <div id="thank-you" class="text-2xl font-inter tracking-tight">
       <span
         class="glowing border-2 border-[#f86aaf] display-block rounded-full py-1 px-3"
         >Thank you</span
       >
-      for your patronage
-    </h2>
+      <span> for your patronage </span>
+    </div>
     <p>An email is on it's way with the details of your order.</p>
     <h1 class="font-antonio font-bold uppercase text-2xl">Order summary:</h1>
   </div>
@@ -36,8 +36,5 @@
   animation-iteration-count: infinite;
   animation-direction: alternate;
   animation-timing-function: ease-in;
-  /*-webkit-box-shadow: 0px 0px 33px 11px rgba(255, 0, 123, 0.9);
-  -moz-box-shadow: 0px 0px 33px 11px rgba(255, 0, 123, 0.9);
-  box-shadow: 0px 0px 33px 11px rgba(255, 0, 123, 0.9);*/
 }
 </style>
