5401b7be0353f62776308ed27212d2bafc3467ec
celoob <carlemilloob@gmail.com>
2024-03-11 13:11:59 +0100

    Checkout styling part 2

--- src/views/checkout.vue
+++ src/views/checkout.vue
@@ -60,33 +60,43 @@ getTotalAmount();
         <p class="text-2xl font-antonio pt-4 pl-4">Checkout</p>
         <form class="font-inter font-light text-[#F5F5F5]">
             <!-- checkout page grid -->
-            <div class="grid p-4 grid-cols-2 grid-rows-3 gap-4 2xl:gap-[1.7vw] font-inter ">
+            <div class="grid p-4 grid-cols-5 grid-rows-3 gap-4 2xl:gap-[1vw] font-inter ">
                 <!-- delivery information div -->
-                <div class="bg-[#1c1c1c]">
+                <div class="bg-[#1c1c1c] col-span-3">
                     <fieldset id="deliveryInfo" ref="deliveryInfo" class="text-[#0c0c0c]">
                         <!-- regex:
                             "[\p{L}a-z]" = letters including swedish/international letters
                             "\s?-?" = optional whitespace or hyphen
                          -->
                         <legend>Delivery Information</legend>
-                        <input id="email" aria-label="Email" type="email" placeholder="Email" class="w-full" required
+                        <input id="email" aria-label="Email" type="email" placeholder="Email" class="w-[48.5%]" required
                             maxlength="40">
-                        <input id="phoneNumber" aria-label="Phone number" type="text" placeholder="Phone number"
+                        <input id="phoneNumber" aria-label="Phone number" type="text" placeholder="Phone number" class="w-[48.5%]"
                             pattern="(\+?(\d\s?-?){11}|(\d\s?-?){10})" required>
-                        <input id="firstName" aria-label="First name" type="text" placeholder="First name"
+                        <input id="firstName" aria-label="First name" type="text" placeholder="First name" class="w-[48.5%]"
                             pattern="[\p{L}a-z]{1,30}" required>
-                        <input id="lastName" aria-label="Last name" type="text" placeholder="Last name"
+                        <input id="lastName" aria-label="Last name" type="text" placeholder="Last name" class="w-[48.5%]"
                             pattern="[\p{L}a-z]{1,30}" required>
-                        <input id="adress" aria-label="Delivery adress" type="text" placeholder="Delivery adress"
+                        <input id="adress" aria-label="Delivery adress" type="text" placeholder="Delivery adress" class="w-[35.5%]"
                             pattern="[\p{L}a-z]{0,30}\s?\d{1,3}" required>
-                        <input id="postCode" aria-label="Post code" type="text" placeholder="Post code" required
+                        <input id="postCode" aria-label="Post code" type="text" placeholder="ZIP code" required class="w-[30%]"
                             pattern="\d{3}\s?\d{2}">
-                        <input id="postTown" aria-label="Post town" type="text" placeholder="Post town" required
+                        <input id="postTown" aria-label="Post town" type="text" placeholder="City" required class="w-[30%]"
                             pattern="[\p{L}a-z]{1,15}">
+                        <button
+                            id="adressButton"
+                            label="Add c/o adress"
+                            class="px-5  mt-3 text-[#003238] bg-[#00e0ff] font-semibold rounded-md hover:opacity-90 font-inter border-y-4 border-[#00000000] disabled:opacity-20 disabled:hover:border-[#ff007a00] relative drop-shadow-[0_4.3px_1.4px_rgba(0,0,0,0.2)]"
+                        >
+                        <div id="button-contents" class="relative p-1.5">
+                            <p class="inline">Add c/o-adress <span class="text-xl font-bold">+</span></p>
+                        </div>
+                        </button>
                     </fieldset>
+
                 </div>
                 <!-- checkout cart div -->
-                <div id="cart" class="bg-[#1c1c1c] row-span-3">
+                <div id="cart" class="bg-[#1c1c1c] row-span-2 col-span-2">
                     <h2 class="text-[#F5F5F5]">Cart</h2>
                     <div class="mt-[2cqh]">
                         <CartCards :editAllowed="false" />
@@ -104,24 +114,24 @@ getTotalAmount();
                 </div>
 
                 <!-- Shipment method div -->
-                <div class="bg-[#1c1c1c]">
+                <div class="bg-[#1c1c1c] col-span-3">
                     <fieldset id="shippingMethod">
                         <legend>Shipping method</legend>
-                        <div class="ring-white ring-2 mt-[2cqh]">
-                            <div class=" ring-white ring-1 flex flex-row items-center">
-                                <input id="postnord" type="radio" value="postnord" name="shippingMethod">
+                        <div class= "mt-[2.5cqh] ">
+                            <div class="flex flex-row items-center border-solid border-[#2a2a2a] border-2">
+                                <input id="postnord" type="radio" value="postnord" name="shippingMethod" class="border-none size-5 ml-5 mr-3" >
                                 <label for="postnord">Postnord</label>
                                 <img src="/src/assets/shipping/postnord-logo.png" alt=""
                                     class="h-16 w-28 object-cover inline ml-auto">
                             </div>
-                            <div class="flex flex-row items-center">
-                                <input id="instabox" type="radio" value="instabox" name="shippingMethod">
+                            <div class="flex flex-row items-center border-solid border-[#2a2a2a] border-x-2">
+                                <input id="instabox" type="radio" value="instabox" name="shippingMethod" class="border-none size-5 ml-5 mr-3">
                                 <label for="instabox">Instabox</label>
                                 <img src="/src/assets/shipping/Instabox-logo.jpg" alt=""
                                     class="h-16 w-28 object-cover inline ml-auto">
                             </div>
-                            <div class="ring-white ring-1 flex flex-row items-center">
-                                <input id="dhl" type="radio" value="dhl" name="shippingMethod">
+                            <div class="flex flex-row items-center border-solid border-[#2a2a2a] border-2">
+                                <input id="dhl" type="radio" value="dhl" name="shippingMethod" class="border-none size-5 ml-5 mr-3">
                                 <label for="dhl">DHL </label>
                                 <img src="/src/assets/shipping/dhl-logo.png" alt=""
                                     class="h-16 w-28 object-cover inline ml-auto">
@@ -130,30 +140,30 @@ getTotalAmount();
                     </fieldset>
                 </div>
                 <!-- payment method div -->
-                <div class="bg-[#1c1c1c]">
+                <div class="bg-[#1c1c1c] col-span-3">
                     <fieldset id="paymentMethod">
                         <legend>Payment method</legend>
-                        <div class="ring-white ring-2 mt-[2cqh]">
-                            <div class="flex flex-row items-center">
-                                <input id="swish" type="radio" value="swish" name="paymentMethod">
+                        <div class="mt-[2cqh]">
+                            <div class="flex flex-row items-center border-solid border-[#2a2a2a] border-2">
+                                <input id="swish" type="radio" value="swish" name="paymentMethod" class="border-none size-5 ml-5 mr-3">
                                 <label for="swish">Swish</label>
                                 <img src="/src/assets/payment/swish-logo.jpg" alt=""
                                     class="h-16 w-28 object-cover inline ml-auto">
                             </div>
-                            <div class="ring-white ring-1 flex flex-row items-center">
-                                <input id="klarna" type="radio" value="klarna" name="paymentMethod">
+                            <div class="flex flex-row items-center border-solid border-[#2a2a2a] border-x-2">
+                                <input id="klarna" type="radio" value="klarna" name="paymentMethod" class="border-none size-5 ml-5 mr-3">
                                 <label for="klarna">Klarna</label>
                                 <img src="/src/assets/payment/klarna-logo.jpg" alt=""
                                     class="h-16 w-28 object-cover inline ml-auto">
                             </div>
-                            <div class="flex flex-row items-center">
-                                <input id="masterCard" type="radio" value="masterCard" name="paymentMethod">
+                            <div class="flex flex-row items-center border-solid border-[#2a2a2a] border-x-2 border-t-2">
+                                <input id="masterCard" type="radio" value="masterCard" name="paymentMethod" class="border-none size-5 ml-5 mr-3">
                                 <label for="masterCard">Mastercard </label>
-                                <img src="/src/assets/payment/mastercard-logo.png" alt=""
-                                    class="h-16 w-28 object-cover inline ml-auto">
+                                <img src="/src/assets/payment/mastercard-logo.svg" alt=""
+                                    class="h-16 w-28 inline ml-auto bg-[#16366F]">
                             </div>
-                            <div class="ring-white ring-1 flex flex-row items-center">
-                                <input id="visa" type="radio" value="visa" name="paymentMethod">
+                            <div class=" flex flex-row items-center border-solid border-[#2a2a2a] border-2">
+                                <input id="visa" type="radio" value="visa" name="paymentMethod" class="border-none size-5 ml-5 mr-3">
                                 <label for="visa">VISA</label>
                                 <img src="/src/assets/payment/visa-logo.jpg" alt=""
                                     class="h-16 w-28 object-cover inline ml-auto">
@@ -197,7 +207,7 @@ h2 {
     padding: 0.5rem;
     margin-top: 2cqh;
     margin-bottom: 1cqh;
-    margin-right: 1cqh;
+    margin-right: 1.5%;
 }
 
 #deliveryInfo>input:focus:placeholder-shown {
