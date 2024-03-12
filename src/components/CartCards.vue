d39d292d5e7a6a549bcda9bd51c223bd98411940
Lucia <lucia.macakova@iths.se>
2024-03-12 10:10:14 +0100

    deleted unnesseccassy class

--- src/components/CartCards.vue
+++ src/components/CartCards.vue
@@ -122,14 +122,9 @@ watch(
             class="flex flex-col justify-between mx-3"
           >
             <h2>{{ productArray[0].price }}:-</h2>
-
-            <h2 v-if="!editAllowed" class="self-center">
-              {{ productArray.length }}
-            </h2>
             <!-- what is intended to display in this H2? Can it be removed? (from Amanda)-->
 
             <img
-              v-if="editAllowed"
               src="/src/assets/icons/trash-icon.svg"
               alt="trashcan icon"
               class="h-7 cursor-pointer m-1 transition-transform transform-gpu hover:scale-[1.1]"
