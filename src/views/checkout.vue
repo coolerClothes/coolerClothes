e85c8c2551c7b674cad7f777605c6c9371e042e5
Lucia <lucia.macakova@iths.se>
2024-03-12 19:11:50 +0100

    add tooltips

--- src/views/checkout.vue
+++ src/views/checkout.vue
@@ -15,7 +15,7 @@ let addCoFields = ref(false);
 
 onMounted(() => {
 
-    const deliveryInfoInputs = Array.from(deliveryInfo.value.getElementsByTagName('input'));
+    let deliveryInfoInputs = Array.from(deliveryInfo.value.getElementsByTagName('input'));
     const shippingMethodFieldset = document.getElementById('shippingMethod');
     const paymentMethodFieldset = document.getElementById('paymentMethod');
 
@@ -53,7 +53,7 @@ onMounted(() => {
             }
         });
     });
-
+    /* Check if paymentMethod fieldset is valid */
     paymentMethodFieldset.querySelectorAll('input[name="paymentMethod"]').forEach(radioButton => {
         radioButton.addEventListener('click', () => {
             if (paymentMethodFieldset.querySelector('input[name="paymentMethod"]:checked') !== null) {
@@ -90,7 +90,6 @@ watch(
 );
 
 getTotalAmount();
-
 </script>
 
 
@@ -101,27 +100,72 @@ getTotalAmount();
             <!-- checkout page grid -->
             <div class="grid p-4 grid-cols-5 grid-rows-20 gap-4 2xl:gap-[1vw] font-inter ">
                 <!-- delivery information div -->
-                <div class="bg-[#1c1c1c] col-span-3 row-span-3 px-6 pt-8 pb-10 border-solid border-l-4 border-[#00e0ff]" :class="{ 'border-solid border-l-4 border-[#4dc94f]': !shipFieldsetDisabled }">
+                <div class="bg-[#1c1c1c] col-span-3 row-span-3 px-6 pt-8 pb-10 border-solid border-l-4 border-[#00e0ff]"
+                    :class="{ 'border-solid border-l-4 border-[#4dc94f]': !shipFieldsetDisabled }">
                     <fieldset id="deliveryInfo" ref="deliveryInfo" class="text-[#0c0c0c]">
                         <!-- regex:
                             "[\p{L}a-z]" = letters including swedish/international letters
                             "\s?-?" = optional whitespace or hyphen
                          -->
                         <legend class="text-[#868686]">Delivery Information</legend>
-                        <input id="email" aria-label="Email" type="email" placeholder="Email" class="w-[48.5%]" required
-                            maxlength="40">
-                        <input id="phoneNumber" aria-label="Phone number" type="text" placeholder="Phone number"
-                            class="w-[48.5%]" pattern="(\+?(\d\s?-?){11}|(\d\s?-?){10})" required>
-                        <input id="firstName" aria-label="First name" type="text" placeholder="First name"
-                            class="w-[48.5%]" pattern="[\p{L}a-z]{1,30}" required>
-                        <input id="lastName" aria-label="Last name" type="text" placeholder="Last name"
-                            class="w-[48.5%]" pattern="[\p{L}a-z]{1,30}" required>
-                        <input id="adress" aria-label="Delivery adress" type="text" placeholder="Delivery adress"
-                            class="w-[35.5%]" pattern="[\p{L}a-z]{0,30}\s?\d{1,3}" required>
-                        <input id="postCode" aria-label="Post code" type="text" placeholder="ZIP code" required
-                            class="w-[30%]" pattern="\d{3}\s?\d{2}">
-                        <input id="postTown" aria-label="Post town" type="text" placeholder="City" required
-                            class="w-[30%]" pattern="[\p{L}a-z]{1,15}">
+                        <div class="relative inline-flex w-[48.5%]">
+                            <input id="email" aria-label="Email" type="email" placeholder="Email" required
+                                maxlength="40" class="w-full">
+                            <p
+                                class="tooltip absolute bottom-[100%] px-1 bg-[#00E0FF] rounded-t-sm self-center text-[#003238] invisible text-xs font-medium z-50 flex">
+                                Email:
+                                example@mail.example <span class="check invisible font-bold absolute right-1">✓</span>
+                                <span class="cross invisible font-bold ml-auto">✗</span>
+                            </p>
+                        </div>
+                        <div class="relative inline-flex w-[48.5%]">
+                            <input id="phoneNumber" aria-label="Phone number" type="text" placeholder="Phone number"
+                                class="w-full" pattern="(\+?(\d\s?-?){11}|(\d\s?-?){10})" required>
+                            <p
+                                class="tooltip absolute bottom-[100%] bg-[#00E0FF] px-1 rounded-t-sm self-center text-[#003238] invisible text-xs font-medium z-50">
+                                Tel: 10 numbers or dialing code followed 11 numbers <span class="check invisible font-bold absolute right-1">✓</span>
+                                <span class="cross invisible font-bold ml-auto">✗</span></p>
+                        </div>
+                        <div class="relative inline-flex w-[48.5%]">
+                            <input id="firstName" aria-label="First name" type="text" placeholder="First name"
+                                class="w-full" pattern="[\p{L}a-z]{1,30}" required>
+                            <p
+                                class="tooltip absolute bottom-[100%] bg-[#00E0FF] px-1 rounded-t-sm self-center text-[#003238] invisible text-xs font-medium z-50">
+                                First name <span class="check invisible font-bold absolute right-1">✓</span>
+                                <span class="cross invisible font-bold ml-auto">✗</span></p>
+                        </div>
+                        <div class="relative inline-flex w-[48.5%]">
+                            <input id="lastName" aria-label="Last name" type="text" placeholder="Last name"
+                                class="w-full" pattern="[\p{L}a-z]{1,30}" required>
+                            <p
+                                class="tooltip absolute bottom-[100%] bg-[#00E0FF] px-1 rounded-t-sm self-center text-[#003238] invisible text-xs font-medium z-50">
+                                Last name <span class="check invisible font-bold absolute right-1">✓</span>
+                                <span class="cross invisible font-bold ml-auto">✗</span></p>
+                        </div>
+                        <div class="relative inline-flex w-[35.5%]">
+                            <input id="adress" aria-label="Delivery adress" type="text" placeholder="Delivery adress"
+                                class="w-full" pattern="[\p{L}a-z]{0,30}\s?\d{1,3}" required>
+                            <p
+                                class="tooltip bg-[#00E0FF] absolute bottom-[100%] px-1 rounded-t-sm self-center text-[#003238] invisible text-xs font-medium z-50">
+                                Adress: Street name + number <span class="check invisible font-bold absolute right-1">✓</span>
+                                <span class="cross invisible font-bold ml-auto">✗</span></p>
+                        </div>
+                        <div class="relative inline-flex w-[30%]">
+                            <input id="postCode" aria-label="Post code" type="text" placeholder="ZIP code" required
+                                class="w-full" pattern="\d{3}\s?\d{2}">
+                            <p
+                                class="tooltip bg-[#00E0FF] absolute bottom-[100%] px-1 rounded-t-sm self-center text-[#003238] invisible text-xs font-medium z-50">
+                                Zip code: 5 numbers <span class="check invisible font-bold absolute right-1">✓</span>
+                                <span class="cross invisible font-bold ml-auto">✗</span></p>
+                        </div>
+                        <div class="relative inline-flex w-[30%]">
+                            <input id="postTown" aria-label="Post town" type="text" placeholder="City" required
+                                class="w-full" pattern="[\p{L}a-z]{1,15}">
+                            <p
+                                class="tooltip bg-[#00E0FF] absolute bottom-[100%] px-1 rounded-t-sm self-center text-[#003238] invisible text-xs font-medium z-50">
+                                Post town <span class="check invisible font-bold absolute right-1">✓</span>
+                                <span class="cross invisible font-bold ml-auto">✗</span></p>
+                        </div>
                         <input v-if="addCoFields === true" id="coAdress" aria-label="c/o adress" type="text"
                             placeholder="c/o adress" class="w-[35.5%]" pattern="[\p{L}a-z]{0,30}\s?\d{1,3}" required>
                         <input v-if="addCoFields === true" id="coPostCode" aria-label="c/o post code" type="text"
@@ -164,7 +208,8 @@ getTotalAmount();
                     </div>
                 </div>
                 <!-- shipment method div -->
-                <div class="bg-[#1c1c1c] col-span-3 row-span-3 px-6 pt-8 pb-10 border-solid border-l-4 border-[#00e0ff]" :class="{ 'opacity-40 filter grayscale': shipFieldsetDisabled }, { 'border-solid border-l-4 border-[#4dc94f]': !payFieldsetDisabled }">
+                <div class="bg-[#1c1c1c] col-span-3 row-span-3 px-6 pt-8 pb-10 border-solid border-l-4 border-[#00e0ff]"
+                    :class="{ 'opacity-40 filter grayscale': shipFieldsetDisabled }, { 'border-solid border-l-4 border-[#4dc94f]': !payFieldsetDisabled }">
                     <fieldset id="shippingMethod" :disabled=shipFieldsetDisabled>
                         <legend class="text-[#868686]">Shipping method</legend>
                         <div>
@@ -196,7 +241,8 @@ getTotalAmount();
                     </fieldset>
                 </div>
                 <!-- payment method div -->
-                <div class="bg-[#1c1c1c] col-span-3 row-span-4 px-6 pt-8 pb-10 border-solid border-l-4 border-[#00e0ff]" :class="{ 'opacity-40 filter grayscale': payFieldsetDisabled }, { 'border-solid border-l-4 border-[#4dc94f]': !confirmFieldsetDisabled }">
+                <div class="bg-[#1c1c1c] col-span-3 row-span-4 px-6 pt-8 pb-10 border-solid border-l-4 border-[#00e0ff]"
+                    :class="{ 'opacity-40 filter grayscale': payFieldsetDisabled }, { 'border-solid border-l-4 border-[#4dc94f]': !confirmFieldsetDisabled }">
                     <fieldset id="paymentMethod" :disabled=payFieldsetDisabled>
                         <legend class="text-[#868686]">Payment method</legend>
                         <div>
@@ -206,7 +252,7 @@ getTotalAmount();
                                 <label for="swish">Swish</label>
                                 <img src="/src/assets/payment/swish-logo.jpg" alt=""
                                     class="h-16 w-28 object-cover inline ml-auto"
-                                    :class="{ 'filter grayscale': payFieldsetDisabled}" >
+                                    :class="{ 'filter grayscale': payFieldsetDisabled }">
                             </div>
                             <div class="flex flex-row items-center border-solid border-[#2a2a2a] border-x-2">
                                 <input id="klarna" type="radio" value="klarna" name="paymentMethod"
@@ -214,7 +260,7 @@ getTotalAmount();
                                 <label for="klarna">Klarna</label>
                                 <img src="/src/assets/payment/klarna-logo.jpg" alt=""
                                     class="h-16 w-28 object-cover inline ml-auto"
-                                    :class="{ 'filter grayscale': payFieldsetDisabled}" >
+                                    :class="{ 'filter grayscale': payFieldsetDisabled }">
                             </div>
                             <div class="flex flex-row items-center border-solid border-[#2a2a2a] border-x-2 border-t-2">
                                 <input id="masterCard" type="radio" value="masterCard" name="paymentMethod"
@@ -222,7 +268,7 @@ getTotalAmount();
                                 <label for="masterCard">Mastercard </label>
                                 <img src="/src/assets/payment/mastercard-logo.svg" alt=""
                                     class="h-16 w-28 inline ml-auto bg-[#16366F]"
-                                    :class="{ 'filter grayscale': payFieldsetDisabled}" >
+                                    :class="{ 'filter grayscale': payFieldsetDisabled }">
                             </div>
                             <div class=" flex flex-row items-center border-solid border-[#2a2a2a] border-2">
                                 <input id="visa" type="radio" value="visa" name="paymentMethod"
@@ -230,7 +276,7 @@ getTotalAmount();
                                 <label for="visa">VISA</label>
                                 <img src="/src/assets/payment/visa-logo.jpg" alt=""
                                     class="h-16 w-28 object-cover inline ml-auto"
-                                    :class="{ 'filter grayscale': payFieldsetDisabled}" >
+                                    :class="{ 'filter grayscale': payFieldsetDisabled }">
                             </div>
                         </div>
                     </fieldset>
@@ -238,7 +284,7 @@ getTotalAmount();
             </div>
             <div class="text-center pb-12" :class="{ 'opacity-40 filter grayscale': confirmFieldsetDisabled }">
                 <fieldset id="confirmPayment" :disabled=confirmFieldsetDisabled>
-                    <input type="checkbox" id="newsLetter" name="News Letter" class="mr-3 size-5 align-middle"/>
+                    <input type="checkbox" id="newsLetter" name="News Letter" class="mr-3 size-5 align-middle" />
                     <label for="newsLetter" class="align-middle text-sm">Sign up to our news letter!</label>
                     <p class="pt-6 font-semibold text-lg">Att betala: {{ totalAmount }}:-</p>
                     <router-link to="/orderConfirmation">
@@ -246,21 +292,12 @@ getTotalAmount();
                             class="px-5  mt-4 text-[#000000] bg-[#4dc94f] font-semibold rounded-md hover:opacity-90 font-inter border-y-4 border-[#00000000] disabled:opacity-20 disabled:hover:border-[#ff007a00] relative drop-shadow-[0_4.3px_1.4px_rgba(0,0,0,0.2)]">
                             <div id="button-contents" class="relative p-1.5">
                                 <p class="inline align-middle">Confirm payment</p>
-                                <svg
-                                    id="added-icon"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    stroke-width="1.5"
-                                    stroke="currentColor"
-                                    class="w-8 h-8 p-1 rounded-full inline transform text-[#000000]"
-                                    >
-                                <!-- animate-bounce duration-1000 -->
-                                <path
-                                    stroke-linecap="round"
-                                    stroke-linejoin="round"
-                                    d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                                />
+                                <svg id="added-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                    stroke-width="1.5" stroke="currentColor"
+                                    class="w-8 h-8 p-1 rounded-full inline transform text-[#000000]">
+                                    <!-- animate-bounce duration-1000 -->
+                                    <path stroke-linecap="round" stroke-linejoin="round"
+                                        d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
                                 </svg>
                             </div>
                         </button>
@@ -289,24 +326,35 @@ h2 {
     color: #858585;
 }
 
-#deliveryInfo>input {
-    background: #f5f5f5;
-    outline: 1px solid #858585;
-    padding-left: 1cqw;
-    padding: 0.4rem;
+#deliveryInfo>div {
     margin-bottom: 2.5%;
     margin-right: 1.5%;
 }
 
-#deliveryInfo>input:focus:placeholder-shown {
-    outline: 3px solid #00E0FF !important;
+#deliveryInfo>div>input {
+    background: #f5f5f5;
+    outline: 1px solid #858585;
+    padding-left: 1cqw;
+    padding: 0.4rem;
 }
 
-#deliveryInfo>input:focus:valid {
-    outline: 3px solid #35ad6d;
+#deliveryInfo>div>input:focus {
+    outline: 3px solid #00E0FF;
 }
 
-#deliveryInfo>input:focus:invalid {
-    outline: 3px solid #c92f53;
+/* Tooltip styling */
+#deliveryInfo>div>input:focus+.tooltip {
+    visibility: visible;
+    color: #003238;
+    background-color: #00E0FF;
+}
+
+/* Tooltip checkmarks and crossmarks styling */
+#deliveryInfo>div>input:focus:invalid+.tooltip>.cross {
+    visibility: visible;
+}
+
+#deliveryInfo>div>input:focus:valid+.tooltip>.check {
+    visibility: visible;
 }
 </style>
